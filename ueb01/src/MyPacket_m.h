2c7c3880c2eb395659432db9db9b9e177e524f10
acereca <acereca@outlook.de>
2020-05-11 16:15:39 +0200

    update README w/ correct build instructions

--- ueb01/src/MyPacket_m.h
+++ ueb01/src/MyPacket_m.h
@@ -1,5 +1,5 @@
 //
-// Generated file, do not edit! Created by nedtool 5.6 from src/MyPacket.msg.
+// Generated file, do not edit! Created by nedtool 5.6 from MyPacket.msg.
 //
 
 #ifndef __MYPACKET_M_H
@@ -19,7 +19,7 @@
 
 
 /**
- * Class generated from <tt>src/MyPacket.msg:19</tt> by nedtool.
+ * Class generated from <tt>MyPacket.msg:19</tt> by nedtool.
  * <pre>
  * //
  * // TODO generated message class
