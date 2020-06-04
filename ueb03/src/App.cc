d91ffc03a77f423cf273ac109e2b772087cd9c1b
acereca <acereca@outlook.de>
2020-06-04 16:05:44 +0200

    setup ueb03 w/ copy of ueb02

---
+++ ueb03/src/App.cc
@@ -0,0 +1,49 @@
+#include "App.h"
+#include "packet_m.h"
+
+Define_Module(App);
+
+void App::initialize() {
+    nodeID = static_cast<int>(par("nodeID"));
+    numNodes = static_cast<int>(par("numNodes"));
+    scheduleAt(simTime(), new cMessage);
+
+    //register stats
+    sigPacketSent = registerSignal("sigPacketSent");
+    sigPacketReceived = registerSignal("sigPacketReceived");
+    sigPacketReceivedSize = registerSignal("sigPacketReceivedSize");
+}
+
+void App::handleMessage(cMessage *msg) {
+    if (msg->isSelfMessage()) {
+        // schedule new self message for clocking and also send packet to random
+        int destination = intuniform(0, numNodes - 1);
+        int64_t packetLength = intuniform(1, 64) * 64; // bytes
+        Packet *pkt = new Packet;
+        pkt->setDestination(destination);
+        pkt->setBitLength(packetLength);
+        send(pkt, "data_out");
+        scheduleAt(simTime() + static_cast<double>(par("sendDelay")), msg);
+        emit(sigPacketSent, true);
+    } else {
+        // not a self message make sure it's a Packet
+        if (dynamic_cast<Packet *>(msg) != nullptr) {
+            EV_INFO << "NodeID " << nodeID << " got a Packet ";
+            Packet *pkt = (Packet *) msg;
+            if (pkt->getDestination() == nodeID) {
+                EV_INFO << "and it was for me" << endl;
+                emit(sigPacketReceivedSize, static_cast<long>(pkt->getBitLength()));
+                delete msg;
+                emit(sigPacketReceived, true);
+            } else {
+                EV_INFO << "but it was not for me sending to next node" << endl;
+                send(pkt, "data_out");
+                emit(sigPacketSent, true);
+            }
+        } else {
+            // not a Packet throw it away
+            EV_INFO << "got non packet message!" << endl;
+            delete msg;
+        }
+    }
+}
---
+++ ueb03/src/App.h
@@ -0,0 +1,22 @@
+#ifndef __UEB02_APP_H_
+#define __UEB02_APP_H_
+
+#include <omnetpp.h>
+
+using namespace omnetpp;
+
+class App : public cSimpleModule
+{
+  protected:
+    virtual void initialize();
+    virtual void handleMessage(cMessage *msg);
+  private:
+    int nodeID;  // own node ID
+    int numNodes; // total number of nodes in Network
+    double sendDelay;
+    simsignal_t sigPacketSent;
+    simsignal_t sigPacketReceived;
+    simsignal_t sigPacketReceivedSize;
+};
+
+#endif
