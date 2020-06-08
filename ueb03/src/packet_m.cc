d6c7fe9e4c1fb352ea5680e01b3829cba375d14c
acereca <acereca@outlook.de>
2020-06-08 10:31:30 +0200

    fix ueb02 to ueb03 renaming and some synax errors in .ned files

--- ueb03/src/packet_m.cc
+++ ueb03/src/packet_m.cc
@@ -1,5 +1,5 @@
 //
-// Generated file, do not edit! Created by nedtool 5.6 from src/packet.msg.
+// Generated file, do not edit! Created by nedtool 5.6 from packet.msg.
 //
 
 // Disable warnings about unused variables, empty switch stmts, etc:
